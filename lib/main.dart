8f1ade924d0818e53bbf2270d96cf2ad430d44af
i.shinkarev <i.shinkarev@z-it.ru>
2024-05-10 15:26:34 +0300

    Add project

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:pytorch_lite_example/run_model_by_camera_demo.dart';
+
+Future<void> main() async {
+  runApp(const ChooseDemo());
+}
+
+class ChooseDemo extends StatefulWidget {
+  const ChooseDemo({Key? key}) : super(key: key);
+
+  @override
+  State<ChooseDemo> createState() => _ChooseDemoState();
+}
+
+class _ChooseDemoState extends State<ChooseDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Car Damage Detector'),
+        ),
+        body: Builder(builder: (context) {
+          return Center(
+            child: Column(
+              children: [
+                TextButton(
+                  onPressed: () => {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const RunModelByCameraDemo()),
+                    )
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                  ),
+                  child: const Text(
+                    "Run Model with Camera",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
